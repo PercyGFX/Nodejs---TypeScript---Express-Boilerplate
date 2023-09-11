88e0dcfb98f391cfd17e3221bcb55d05afe88c98
PercyGFX <isurangabtk@gmail.com>
2023-09-12 02:10:56 +0530

    boilerplate with express nodemon

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express()
+
+app.get('/',(req,res)=>{
+
+    res.send("hello")
+})
+
+
+const port = process.env.PORT || 5000; // Default to port 3000 if PORT environment variable is not set
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
